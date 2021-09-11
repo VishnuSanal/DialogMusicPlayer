1a575efc522ae3ab0e248c9010ce86f0245147f2
Vishnu Sanal. T <t.v.s10123@gmail.com>
2021-09-11 19:46:24 +0530

    MVP

--- app/src/main/java/phone/vishnu/musicplayer/MainActivity.java
+++ app/src/main/java/phone/vishnu/musicplayer/MainActivity.java
@@ -1,8 +1,13 @@
 package phone.vishnu.musicplayer;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Intent intent = getIntent();
+
+        if (
+                Intent.ACTION_VIEW.equals(intent.getAction()) &&
+                        intent.getData() != null
+        )
+            playAudio(
+                    intent.getData().getPath().replace("/storage_root/", "")
+            );
+
+    }
+
+    public void playAudio(String path) {
+        MediaPlayer mediaPlayer = new MediaPlayer();
+
+        try {
+
+            mediaPlayer.setDataSource(path);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Oops! Something went wrong\n\n" + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
