048f536b50273c1bbabf698ab0a0a6952af03837
Vishnu Sanal. T <t.v.s10123@gmail.com>
2021-09-11 21:34:23 +0530

    SeekBar

--- app/src/main/java/phone/vishnu/musicplayer/MainActivity.java
+++ app/src/main/java/phone/vishnu/musicplayer/MainActivity.java
@@ -3,44 +3,73 @@ package phone.vishnu.musicplayer;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        seekBar = findViewById(R.id.seekBar);
+
         final Intent intent = getIntent();
 
-        if (
-                Intent.ACTION_VIEW.equals(intent.getAction()) &&
-                        intent.getData() != null
-        )
-            playAudio(
-                    intent.getData().getPath().replace("/storage_root/", "")
-            );
+        if (Intent.ACTION_VIEW.equals(intent.getAction()) && intent.getData() != null) {
 
-    }
+            String path = intent.getData().getPath().replace("/storage_root/", "");
 
-    public void playAudio(String path) {
-        MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer = new MediaPlayer();
 
-        try {
+            try {
+                mediaPlayer.setDataSource(path);
+                mediaPlayer.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Oops! Something went wrong\n\n" + e.toString(), Toast.LENGTH_SHORT).show();
+            }
 
-            mediaPlayer.setDataSource(path);
-            mediaPlayer.prepare();
+            seekBar.setMax(mediaPlayer.getDuration());
+
             mediaPlayer.start();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Oops! Something went wrong\n\n" + e.toString(), Toast.LENGTH_SHORT).show();
+            new Timer().scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                }
+            }, 0, 1);
+
         }
 
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (mediaPlayer != null && fromUser)
+                    mediaPlayer.seekTo(progress);
+            }
+        });
     }
 }
