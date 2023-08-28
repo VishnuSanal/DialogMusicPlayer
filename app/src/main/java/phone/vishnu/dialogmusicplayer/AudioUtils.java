d0198a505a5ffd52327ba89cf529dbfeee4bdc52
VishnuSanal <t.v.s10123@gmail.com>
2023-08-28 20:35:05 +0530

    update AudioUtils

--- app/src/main/java/phone/vishnu/dialogmusicplayer/AudioUtils.java
+++ app/src/main/java/phone/vishnu/dialogmusicplayer/AudioUtils.java
@@ -23,78 +23,37 @@ import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
 import android.media.MediaMetadata;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
 import android.support.v4.media.MediaMetadataCompat;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AudioUtils {
 
     public static Audio getMetaData(Context context, String duration, Uri uri) {
 
-        try (Cursor cursor =
-                context.getApplicationContext()
-                        .getContentResolver()
-                        .query(
-                                Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
-                                        ? MediaStore.Audio.Media.getContentUri(
-                                                MediaStore.VOLUME_EXTERNAL)
-                                        : MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                                new String[] {
-                                    MediaStore.Video.Media._ID,
-                                    MediaStore.Video.Media.DISPLAY_NAME,
-                                    MediaStore.Video.Media.ARTIST,
-                                    MediaStore.Video.Media.DURATION,
-                                },
-                                MediaStore.Video.Media.DURATION + " = ?",
-                                new String[] {duration},
-                                null)) {
+        try {
 
-            int idColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID);
+            Audio audio = fetchMetadata(context, duration, uri);
 
-            int nameColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME);
+            if (audio != null) return audio;
 
-            int durationColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION);
+            AtomicReference<Uri> contentUri = new AtomicReference<>();
 
-            int artistColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.ARTIST);
+            MediaScannerConnection.scanFile(
+                    context,
+                    new String[] {uri.getPath()},
+                    null,
+                    (s, resultUri) -> contentUri.set(resultUri));
 
-            //noinspection LoopStatementThatDoesntLoop
-            while (cursor.moveToNext()) {
+            audio = fetchMetadata(context, duration, contentUri.get());
 
-                long id = cursor.getLong(idColumn);
+            if (audio != null) return audio;
 
-                Uri contentUri =
-                        ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id);
-
-                int d = cursor.getInt(durationColumn);
-
-                String name = cursor.getString(nameColumn);
-
-                if (name != null) {
-                    int index = name.lastIndexOf(".");
-
-                    if (index > -1) name = name.substring(0, index);
-                }
-
-                if (name == null || name.equals("<unknown>")) name = extractName(uri);
-
-                String artist = cursor.getString(artistColumn);
-
-                if (artist == null || artist.equals("<unknown>")) artist = "<Unknown Artist>";
-
-                return new Audio(
-                        id,
-                        new MediaMetadataCompat.Builder()
-                                .putString(MediaMetadata.METADATA_KEY_MEDIA_ID, String.valueOf(id))
-                                .putString(MediaMetadata.METADATA_KEY_DISPLAY_TITLE, name)
-                                .putString(MediaMetadata.METADATA_KEY_ARTIST, artist)
-                                .putLong(
-                                        MediaMetadata.METADATA_KEY_DURATION,
-                                        Long.parseLong(duration))
-                                .build(),
-                        d,
-                        contentUri);
-            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
         }
 
         return new Audio(
@@ -107,6 +66,78 @@ public class AudioUtils {
                         .build(),
                 Long.parseLong(duration),
                 uri);
+    }
+
+    private static Audio fetchMetadata(Context context, String duration, Uri uri)
+            throws IllegalArgumentException {
+
+        Cursor cursor =
+                context.getApplicationContext()
+                        .getContentResolver()
+                        .query(
+                                Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+                                        ? MediaStore.Audio.Media.getContentUri(
+                                                MediaStore.VOLUME_EXTERNAL)
+                                        : MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                                new String[] {
+                                    MediaStore.Audio.Media._ID,
+                                    MediaStore.Audio.Media.DISPLAY_NAME,
+                                    MediaStore.Audio.Media.ARTIST,
+                                    MediaStore.Audio.Media.DURATION,
+                                },
+                                MediaStore.Audio.Media.DURATION + " = ?",
+                                new String[] {duration},
+                                null);
+
+        if (cursor == null) return null;
+
+        int idColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID);
+
+        int nameColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME);
+
+        int durationColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION);
+
+        int artistColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.ARTIST);
+
+        //noinspection LoopStatementThatDoesntLoop
+        while (cursor.moveToNext()) {
+
+            long id = cursor.getLong(idColumn);
+
+            Uri contentUri =
+                    ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id);
+
+            int d = cursor.getInt(durationColumn);
+
+            String name = cursor.getString(nameColumn);
+
+            if (name != null) {
+                int index = name.lastIndexOf(".");
+
+                if (index > -1) name = name.substring(0, index);
+            }
+
+            if (name == null || name.equals("<unknown>")) name = extractName(uri);
+
+            String artist = cursor.getString(artistColumn);
+
+            if (artist == null || artist.equals("<unknown>")) artist = "<Unknown Artist>";
+
+            cursor.close();
+
+            return new Audio(
+                    id,
+                    new MediaMetadataCompat.Builder()
+                            .putString(MediaMetadata.METADATA_KEY_MEDIA_ID, String.valueOf(id))
+                            .putString(MediaMetadata.METADATA_KEY_DISPLAY_TITLE, name)
+                            .putString(MediaMetadata.METADATA_KEY_ARTIST, artist)
+                            .putLong(MediaMetadata.METADATA_KEY_DURATION, Long.parseLong(duration))
+                            .build(),
+                    d,
+                    contentUri);
+        }
+
+        return null;
     }
 
     private static String extractName(Uri uri) {
